a0debcdc2087dfbaa71847766e53a640209773b8
miga-choi <miga.choi.1004@gmail.com>
2024-08-09 00:08:55 +0900

    try "predefined values"

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from enum import Enum
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -31,3 +39,14 @@ async def read_users():
 @app.get("/users")
 async def read_users2():
     return ["Bean", "Elfo"]
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
