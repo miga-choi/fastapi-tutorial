f9df31398982af209c85409d732cf1bd99bbff13
miga-choi <miga.choi.1004@gmail.com>
2024-08-10 01:37:29 +0900

    try more "query parameters"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 from fastapi import FastAPI
 
 
+# Predefined values
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -18,11 +19,15 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
+# Optional parameters
+@app.get("/items/optional/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
 
 
+# Query parameter type conversion
 @app.get("/items/conversion/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
     item = {"item_id": item_id}
@@ -35,38 +40,80 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
     return item
 
 
-@app.get("/items/optional/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+# Required query parameters
+@app.get("/items/required/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
 
 
-@app.get("/items/{item_id}")
+# Required + Default value + Optional
+@app.get("/items/multiple/{item_id}")
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+# Path parameters with types
+@app.get("/items/types/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
 
 
+# Path Parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+# Query Parameters
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Order matters 1-1
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
 
+# Order matters 1-2
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
 
 
+# Order matters 2-1
 @app.get("/users")
 async def read_users():
     return ["Rick", "Morty"]
 
 
+# Order matters 2-2
 @app.get("/users")
 async def read_users2():
     return ["Bean", "Elfo"]
 
 
+# Declare a path parameter + Working with Python enumerations
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -78,6 +125,7 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
+# Path parameters containing paths
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
