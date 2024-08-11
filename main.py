7eaddcd1a6c4bc3f5202ae4f33700e64059c30e4
miga-choi <miga.choi.1004@gmail.com>
2024-08-11 20:43:55 +0900

    try "request body"

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 # Predefined values
@@ -12,6 +20,15 @@ class ModelName(str, Enum):
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 @app.get("/")
