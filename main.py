22b0f23af17c02e9154adb7f74c2368de4ad3542
miga-choi <miga.choi.1004@gmail.com>
2024-08-13 23:15:39 +0900

    try "path parameters"

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from enum import Enum
-from fastapi import FastAPI, Query
-from pydantic import BaseModel, Required
-from typing import List, Union
+from fastapi import FastAPI
 
 
-# Pydantic's BaseModel
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-# Predefined values
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -21,168 +10,14 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+# Path parameters containing paths
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
 
 
-# Query parameter list / multiple values
-@app.get("/items/multiple/validations")
-async def read_items(q: List[str] = Query(
-    default=["foo", "bar"],
-    min_length=3,
-    max_length=50,
-    pattern="^fixedquery$",
-    title="Query string",
-    description="Query string for the items to search in the database that have a good match",
-    alias="item-query",
-    deprecated=True,
-)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-
-# Query Parameters and String Validations
-@app.get("/items/validations")
-async def read_items(
-        q: Union[str, None] = Query(
-            default=Required,
-            min_length=3,
-            max_length=50,
-            pattern="^fixedquery$"
-        )
-):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-
-# Request body + path parameters
-@app.post("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_id": item_id, **item.model_dump()}
-
-
-# Request body + path + query parameters
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q": q})
-    return result
-
-
-# Request Body
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-# Optional parameters
-@app.get("/items/optional/{item_id}")
-async def read_item(item_id: str, q: str | None = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-
-# Query parameter type conversion
-@app.get("/items/conversion/{item_id}")
-async def read_item(item_id: str, q: str | None = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
-# Required query parameters
-@app.get("/items/required/{item_id}")
-async def read_user_item(item_id: str, needy: str):
-    item = {"item_id": item_id, "needy": needy}
-    return item
-
-
-# Required + default value + optional
-@app.get("/items/multiple/{item_id}")
-async def read_user_item(
-        item_id: str, needy: str, skip: int = 0, limit: int | None = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
-
-
-# Path parameters with types
-@app.get("/items/types/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-# Path Parameters
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
-
-
-# Query Parameters
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip: skip + limit]
-
-
-# Multiple path and query parameters
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(
-        user_id: int, item_id: str, q: str | None = None, short: bool = False
-):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
-    return item
-
-
-# Order matters 1-1
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-# Order matters 1-2
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-# Order matters 2-1
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
-
-
-# Order matters 2-2
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
-
-
-# Declare a path parameter + working with python enumerations
+# Predefined values
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
@@ -194,7 +29,30 @@ async def get_model(model_name: ModelName):
     return {"model_name": model_name, "message": "Have some residuals"}
 
 
-# Path parameters containing paths
-@app.get("/files/{file_path:path}")
-async def read_file(file_path: str):
-    return {"file_path": file_path}
+# Order matters 1
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+# Order matters 2
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+# Path Parameters with types
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+# Path Parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
