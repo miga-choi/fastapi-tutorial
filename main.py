ef2f95701b3b72f0b02600a1d75780c2c1879345
miga-choi <miga.choi.1004@gmail.com>
2024-08-13 23:29:55 +0900

    try "query parameters"

--- main.py
+++ main.py
@@ -10,6 +10,79 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# Required parameters + Optional parameters
+@app.get("/items/{item_id}")
+async def read_user_item(
+        item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+# Required query parameters
+# try:
+#   http://127.0.0.1:8000/items/foo-item
+#   http://127.0.0.1:8000/items/foo-item?needy=sooooneedy
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Query parameter type conversion
+# try:
+#   http://127.0.0.1:8000/items/foo?short=1
+#   http://127.0.0.1:8000/items/foo?short=True
+#   http://127.0.0.1:8000/items/foo?short=true
+#   http://127.0.0.1:8000/items/foo?short=on
+#   http://127.0.0.1:8000/items/foo?short=yes
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# Optional parameters
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+
+# Query Parameters
+# try:
+#   http://127.0.0.1:8000/items/
+#   http://127.0.0.1:8000/items/?skip=0&limit=10
+#   http://127.0.0.1:8000/items/?skip=20
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
 
 # Path parameters containing paths
 @app.get("/files/{file_path:path}")
