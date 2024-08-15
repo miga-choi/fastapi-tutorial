f988736c5295696bd4529a221044b57e26c324e7
miga-choi <miga.choi.1004@gmail.com>
2024-08-15 21:02:51 +0900

    try "body - multiple parameters"

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from fastapi import FastAPI, Path, Query
+from fastapi import Body, FastAPI, Path, Query
 from pydantic import BaseModel
 from typing import Annotated, List
 
@@ -17,9 +17,62 @@ class ModelName(str, Enum):
     lenet = "lenet"
 
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# Embed a single body parameter
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+
+# Multiple body params and query
+@app.put("/items/{item_id}")
+async def update_item(
+        *,
+        item_id: int,
+        item: Item,
+        user: User,
+        importance: Annotated[int, Body(gt=0)],
+        q: str | None = None,
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# Singular values in body
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+
+# Body - Multiple Parameters
+# Mix Path, Query and body parameters
+@app.put("/items/{item_id}")
+async def update_item(
+        item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+        q: str | None = None,
+        item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
 
 
 # Number validations: floats, greater than and less than
